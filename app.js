af3e6ca8768edfead895d8794e1031ee7eb14205
lionelbergen@live.com <lionelbergen@live.com>
2018-11-18 10:37:13 -0600

    gg

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
+let express = require('express'),
 let faye = require('faye');
 let http = require('http');
 
+let app = express();
+let server = http.createServer(app);
 let bayeux = new faye.NodeAdapter({mount: '/'});
-let server = http.createServer();
 
-bayeux.attach(http);
-http.listen(8000);
+bayeux.attach(server);
+
+bayeux.on('handshake', function(clientId) {
+    console.log('Client connected', clientId);
+});
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/test.html');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.send(500);
+});
+
+var port = process.env.PORT || 8000;
+server.listen(port, function() {
+    console.log('Listening on ' + port);
+});
