478968592df2f362bc694724de1142446c8c5d57
unknown <lionelBergen@live.com>
2020-01-11 23:16:17 -0600

    removed unused file

--- CommonTools.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = function() {
-	this.GetSecondsSinceUTCTimestamp = getSecondsSinceUTCTimestamp;
-};
-
-function getSecondsSinceUTCTimestamp(utcTimestamp)
-{
-	return (Date.now() - new Date(utcTimestamp * 1000).getTime()) / 1000;
-}
