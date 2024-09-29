89fd4cc441522fa5014577ca5bcb0d210d803ab2
dustytrash <Lionelbergen@live.com>
2024-09-29 12:50:06 -0500

    Version 2, eslint 2016, async functions, updated dependencies ETC

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-// Local files
-const rClient = require('./RedditClient.js');
+import { CreateAuthedClient } from 'reddit-simple-client';
 
-const express = require('express');
-const Faye = require('faye');
-const http = require('http');
+import express from 'express';
+import Faye from 'faye';
+import http from 'http';
 
 // Re-auth every hour
 const REAUTHENTICATE_REDDIT = 1000 * 60 * 60;
@@ -17,95 +16,98 @@ if (!process.env.REDDIT_LOGIN_USERNAME || !process.env.REDDIT_LOGIN_PASSWORD) {
   throw 'REDDIT_LOGIN_USERNAME && REDDIT_LOGIN_PASSWORD environment variables must be set!';
 }
 
-// Note the last argument is the start function
-const RedditClient = new rClient(process.env.REDDIT_LOGIN_USERNAME, process.env.REDDIT_LOGIN_PASSWORD, start);
+const redditAuth = {
+  username: process.env.REDDIT_USERNAME,
+  password: process.env.REDDIT_PASSWORD,
+  appId: process.env.REDDIT_APP_ID,
+  appSecret: process.env.REDDIT_APP_SECRET,
+  redirectUrl: 'https://github.com/LionelBergen/reddit-agree-with-you',
+  accessToken: null,
+  userAgent: 'u/agree-with-you reddit-agree-with-you/2.0.0 by Lionel Bergen'
+};
 
 const app = express();
 const server = http.createServer(app);
-const bayeux = new Faye.NodeAdapter({mount: '/', timeout: 45});
+const bayeux = new Faye.NodeAdapter({ mount: '/', timeout: 45 });
 
 const port = process.env.PORT || 8000;
 
 let lastSentAt = new Date().getTime();
-let pooledCommentsToReplyTo = [];
+const pooledCommentsToReplyTo = [];
 
 const client = new Faye.Client('http://localhost: ' + port + '/faye');
 
 bayeux.attach(server);
 
 bayeux.on('handshake', function(clientId) {
-    console.log('Client connected', clientId);
+  console.log('Client connected', clientId);
 });
 
 // Listen for a message containing a comment & reply message to process
 server.listen(port, function() {
-    console.log('Listening on port: ' + port);
+  console.log('Listening on port: ' + port);
 });
 
-function start()
-{
+function startIntervals(RedditClient) {
+  const intervalFunction = () => {
+    postFromPooledComments(RedditClient);
+  };
+
   // If too many comments come in at once, add them to a pool to be processed during quiet periods
-	setInterval(postFromPooledComments, SECS_PROCESSED_A_POOLED_COMMENT);
-  
-	// Renew auth every hour. 
-	setInterval(function() {
-    if (renewRedditAuth) 
-    {
-      RedditClient.getAuth()
-    } 
-    
+  setInterval(intervalFunction, SECS_PROCESSED_A_POOLED_COMMENT);
+
+  // Renew auth every hour.
+  setInterval(function() {
+    if (renewRedditAuth) {
+      RedditClient.getAuth();
+    }
+
     renewRedditAuth = true;
   }, REAUTHENTICATE_REDDIT);
-  
-	subscribeAndStartPostingComments();
 }
 
-function subscribeAndStartPostingComments()
-{
-	client.subscribe('/messages', function (newMessage) {
-		if (newMessage.comment)
-		{
-			reply = newMessage.reply;
-			newMessage = newMessage.comment;
-			
-			if (pooledCommentsToReplyTo.length == 0 && new Date().getTime() - lastSentAt > MINIMUM_TIME_BETWEEN_REDDIT_COMMENTS)
-			{
-				processComment(newMessage, reply);
-			}
-			else
-			{
-				pooledCommentsToReplyTo.push({name: newMessage.name, reply: reply});
-			}
-		}
-		else
-		{
-			console.log(newMessage);
-		}
-	});
+function subscribeAndStartProcessingComments(RedditClient) {
+  client.subscribe('/messages', function(newMessage) {
+    if (newMessage.comment) {
+      reply = newMessage.reply;
+      newMessage = newMessage.comment;
+
+      if (pooledCommentsToReplyTo.length == 0
+        && new Date().getTime() - lastSentAt > MINIMUM_TIME_BETWEEN_REDDIT_COMMENTS) {
+        processComment(RedditClient, newMessage, reply);
+      } else {
+        pooledCommentsToReplyTo.push({ name: newMessage.name, reply: reply });
+      }
+    } else {
+      console.log(newMessage);
+    }
+  });
 }
 
-function postFromPooledComments()
-{
-	if (pooledCommentsToReplyTo.length > 0 && getCurrentTime() - lastSentAt > MINIMUM_TIME_BETWEEN_REDDIT_COMMENTS)
-	{
-		let comment = pooledCommentsToReplyTo[0];
-		
+function postFromPooledComments(RedditClient) {
+  if (pooledCommentsToReplyTo.length > 0 && getCurrentTime() - lastSentAt > MINIMUM_TIME_BETWEEN_REDDIT_COMMENTS) {
+    const comment = pooledCommentsToReplyTo[0];
+
     // Process the comment and remove it from our pool
-		processComment(comment, comment.reply);
-		pooledCommentsToReplyTo.splice(0, 1);
-	}
+    processComment(RedditClient, comment, comment.reply);
+    pooledCommentsToReplyTo.splice(0, 1);
+  }
 }
 
 /**
  * Posts the reply to a comment and sets 'lastSendAt' variable to the current time
 */
-function processComment(comment, reply)
-{
-	RedditClient.postComment(comment.name, reply);
-	lastSentAt = getCurrentTime();
+function processComment(RedditClient, comment, reply) {
+  RedditClient.postComment(comment.name, reply);
+  lastSentAt = getCurrentTime();
 }
 
-function getCurrentTime()
-{
+function getCurrentTime() {
   return new Date().getTime();
 }
+
+(async () => {
+  const RedditClient = await CreateAuthedClient({ redditAuth });
+  startIntervals(RedditClient);
+  subscribeAndStartProcessingComments(RedditClient);
+})();
---
+++ node_modules/eslint-config-eslinter/README.md
@@ -0,0 +1,11 @@
+eslinter
+--------   
+Project used to enforce common eslint rules such as whitespace, good practices etc  
+
+### Usage  
+In your eslintrc file you can extend like this:  
+```
+module.exports = {
+  extends: ["eslinter"]
+};
+```  
